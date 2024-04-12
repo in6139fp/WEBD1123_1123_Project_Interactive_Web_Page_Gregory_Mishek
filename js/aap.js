798ebd91e5580007a7748d807f15b8bcf50c7a37
in6139fp <in6139fp@go.minnstate.edu>
2024-04-12 10:18:29 -0500

    comments

--- js/aap.js
+++ js/aap.js
@@ -3,6 +3,7 @@ let container = document.getElementById("container");
 let square = document.querySelectorAll(".square");
 
 /*console.log(square);*/
+/* trying to add text title and a square*/
 
 
 
