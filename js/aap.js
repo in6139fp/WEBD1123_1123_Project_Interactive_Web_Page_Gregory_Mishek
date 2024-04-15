949b277cde8be1718745fec9bbdb1f2bd6927cd8
in6139fp <in6139fp@go.minnstate.edu>
2024-04-15 16:07:06 -0500

    review

--- js/aap.js
+++ js/aap.js
@@ -1,8 +1,11 @@
 gsap.registerPlugin(ScrollTrigger);
+
 let container = document.getElementById("container");
 let square = document.querySelectorAll(".square");
 
+
 /*console.log(square);*/
+
 /* trying to add text title and a square*/
 
 
